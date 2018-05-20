f274a36c251f160f12c1053a9f9478d45b781041
Lego <bitarluan@gmail.com>
2018-05-19 22:45:44 -0400

    adding pwa and facebook sharing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,21 @@
-import { UsersModule } from './modules/users/users.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { LayoutModule } from '@angular/cdk/layout';
+import { ErrorStateMatcher } from '@angular/material';
 
 import { AngularFireModule } from 'angularfire2';
 import { AngularFireDatabaseModule } from 'angularfire2/database';
 import { AngularFirestoreModule } from 'angularfire2/firestore';
 import { AngularFireAuthModule } from 'angularfire2/auth';
 
+import { UsersModule } from './modules/users/users.module';
 import { environment } from './../environments/environment';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AuthModule } from './modules/auth/auth.module';
 import { SharedModule } from './modules/shared/shared.module';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { LayoutModule } from '@angular/cdk/layout';
-import { ErrorStateMatcher } from '@angular/material';
 import { ErrorMatcher } from './utils/error-matcher.utils';
 
 @NgModule({
@@ -30,7 +31,8 @@ import { ErrorMatcher } from './utils/error-matcher.utils';
     AngularFireAuthModule,
     SharedModule,
     AuthModule,
-    UsersModule
+    UsersModule,
+    ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [
     {
